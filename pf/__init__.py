5ecc2acfd52a44b767931606364bf317096b5ce9
tmthydvnprt <tim@tmthydvnprt.com>
2016-03-06 09:49:04 -0800

    add base files and folders

---
+++ pf/__init__.py
@@ -0,0 +1,31 @@
+"""
+pf
+
+Personal Finance library for the Computationally Curious.
+
+project    : pf
+version    : 0.0.0
+status     : development
+modifydate :
+createdate :
+website    : https://github.com/tmthydvnprt/pf
+author     : tmthydvnprt
+email      : tim@tmthydvnprt.com
+maintainer : tmthydvnprt
+license    : MIT
+copyright  : Copyright 2016, tmthydvnprt
+credits    :
+
+"""
+
+__all__ = ["pf"]
+__version__ = '0.0.0'
+__date__ = '2016-03-06 09:30:00 -0700'
+__author__ = 'tmthydvnprt'
+__status__ = 'development'
+__website__ = 'https://github.com/tmthydvnprt/pf'
+__email__ = 'tim@tmthydvnprt.com'
+__maintainer__ = 'tmthydvnprt'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016, tmthydvnprt'
+__credits__ = ''
