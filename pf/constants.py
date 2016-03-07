f24695ec148955b611b04f191d3f8c77d68fbb54
tmthydvnprt <tim@tmthydvnprt.com>
2016-03-07 05:51:11 -0800

    add constants file

---
+++ pf/constants.py
@@ -0,0 +1,27 @@
+"""
+constants.py
+
+Constants used across code.
+
+project    : pf
+version    : 0.0.0
+status     : development
+modifydate :
+createdate :
+website    : https://github.com/tmthydvnprt/pf
+author     : tmthydvnprt
+email      : tim@tmthydvnprt.com
+maintainer : tmthydvnprt
+license    : MIT
+copyright  : Copyright 2016, tmthydvnprt
+credits    :
+
+"""
+
+import re
+
+################################################################################################################################
+# Constant Definitions
+################################################################################################################################
+DAYS_IN_YEAR = 365.24
+DATE_RE = re.compile(r'\d{4}-\d{2}-\d{2}')
