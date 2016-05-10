030c35325c6ea3b779b1492a98f6c77e19084d39
tmthydvnprt <tim@tmthydvnprt.com>
2016-05-10 04:55:15 -0700

    add read transactions

--- pf/io.py
+++ pf/io.py
@@ -21,7 +21,7 @@ credits    :
 ################################################################################################################################
 # Data Input Functions
 ################################################################################################################################
-def clean_accounts(accounts):
+def clean_accounts(accounts=None):
     """
     Clean account data for any reason necessary. This is called at the end of `read_in_accounts()`. This is optional and will
     always need to be overriden by the user.
@@ -54,13 +54,70 @@ def read_in_accounts(filepath=''):
     accounts = xlxs['accounts']
     limits = xlxs['limits']
     loan = xlxs['loan']
+    taxes = xlxs['taxes']
 
     # Set Index name for later
     accounts.index.name = 'Date'
     limits.index.name = 'Date'
     loan.index.name = 'Date'
+    taxes.index.name = 'Date'
 
     # Clean up account data by user
     accounts = clean_accounts(accounts)
 
-    return (accounts, limits, loan)
+    return (accounts, limits, loan, taxes)
+
+def clean_transactions(transactions=None):
+    """
+    Cleans transaction data at the end of `read_in_transactions()`, this will always need to be overriden by the user.
+    """
+
+    # Group Investments transactions together for simplicity
+    # transactions.loc[transactions['account'] == 'Motif Individual', 'merchant'] = 'Motif Individual'
+
+    # Remove Pending and Schedualed transactions
+    # drop = (
+    #     (transactions['account'] == 'Student Loan 1') | (transactions['account'] == 'Student Loan 2')
+    # ) & (transactions['omerchant'] != 'Payment')
+    # transactions = transactions[~drop]
+
+    # Rename Category
+    # transactions.loc[transactions['category'] == 'Transfer for Cash Spending', 'category'] = u'Cash Spending'
+
+    return transactions
+
+def read_in_transactions(filepath=''):
+    """
+    Read in the data file containing all transaction details, this should be a json file from mint.com, or the function should
+    overriden by the user.
+
+    Example:
+    ```
+    transactions = read_in_transactions('/path/to/transactions.json')
+    ```
+
+    See readme for example of [mint](mint.com) json export
+
+    """
+
+    # Read Transaction info
+    transactions = pd.read_json(filepath, orient='records').set_index('date')
+    # Correct Dates for index
+    transactions.index = pd.DatetimeIndex([parse_month_day_dates(d) for d in transactions.index], name='Date')
+
+    # Process labels
+    transactions['labels'] = [{label['name'] for label in transaction} for transaction in transactions['labels']]
+
+    # Remove duplicate transactions
+    transactions = transactions[np.invert(transactions.isDuplicate)]
+
+    # Convert amount from dollar strings to floats
+    transactions['amount'] = transactions['amount'].str.replace('$', '').str.replace(',', '').astype(float)
+
+    # Set debit transactions as negative
+    transactions.loc[transactions.isDebit, 'amount'] = -1.0 * transactions.loc[transactions.isDebit, 'amount']
+
+    # Clean up transaction data by user
+    transactions = clean_transactions(transactions)
+
+    return transactions
