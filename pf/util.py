812a81a53f6bf958ff67980ba0a97d6d1f8558ad
tmthydvnprt <tim@tmthydvnprt.com>
2016-05-28 09:39:32 -0700

    add progress bar and model pdf generator

--- pf/util.py
+++ pf/util.py
@@ -53,3 +53,61 @@ def f2as(x=0.0):
         return ' '
     else:
         return '{:0,.2f}'.format(x) if x >= 0 else '({:0,.2f})'.format(np.abs(x))
+
+class ProgressBar(object):
+    """implements a comand-line progress bar"""
+
+    def __init__(self, iterations):
+        """create a progress bar"""
+        self.iterations = iterations
+        self.prog_bar = '[]'
+        self.fill_char = '*'
+        self.width = 40
+        self.__update_amount(0)
+
+    def animate(self, iterate):
+        """animate progress"""
+        print '\r', self,
+        sys.stdout.flush()
+        self.update_iteration(iterate + 1)
+        return self
+
+    def update_iteration(self, elapsed_iter):
+        """increment progress"""
+        self.__update_amount((elapsed_iter / float(self.iterations)) * 100.0)
+        self.prog_bar = '%s  %s of %s complete' % (self.prog_bar, elapsed_iter, self.iterations)
+        return self
+
+    def __update_amount(self, new_amount):
+        """update amout of progress"""
+        percent_done = int(round((new_amount / 100.0) * 100.0))
+        all_full = self.width - 2
+        num_hashes = int(round((percent_done / 100.0) * all_full))
+        self.prog_bar = '[%s%s]' % ((self.fill_char * num_hashes), ' ' * (all_full - num_hashes))
+        pct_place = (len(self.prog_bar) // 2) - len(str(percent_done))
+        pct_string = '%s%%' % (percent_done)
+        self.prog_bar = '%s%s%s' % (self.prog_bar[0:pct_place], pct_string, self.prog_bar[pct_place + len(pct_string):])
+        return self
+
+    def __str__(self):
+        """string representation"""
+        return str(self.prog_bar)
+
+def make_pdf(model, params, size=10000):
+    """Generate model's Propbability Distribution Function """
+
+    # Separate parts of parameters
+    arg = params[:-2]
+    loc = params[-2]
+    scale = params[-1]
+
+    # Get sane start and end points of distribution
+    start = model.ppf(0.01, *arg, loc=loc, scale=scale) if arg else model.ppf(0.01, loc=loc, scale=scale)
+    end = model.ppf(0.99, *arg, loc=loc, scale=scale) if arg else model.ppf(0.99, loc=loc, scale=scale)
+
+    # Build PDF and turn into pandas Series
+    x = np.linspace(start, end, size)
+    y = model.pdf(x, loc=loc, scale=scale, *arg)
+    pdf = pd.Series(y, x)
+
+    return pdf
