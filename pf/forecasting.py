66891344e77a10707f4c4406e4043d2f69f03491
tmthydvnprt <tim@tmthydvnprt.com>
2016-06-05 18:13:30 -0700

    Improve assumption forecasting

--- pf/forecasting.py
+++ pf/forecasting.py
@@ -37,6 +37,8 @@ def assumption_fi_forecast(
         return_rate=0.05,
         age=23,
         life_expectancy=90,
+        min_spending=0,
+        max_spending=9e999,
         start=None,
         expense_increase=True
     ):
@@ -45,19 +47,20 @@ def assumption_fi_forecast(
     """
 
     # Calculate years to simulate
-    years = life_expectancy - age
+    years = (life_expectancy - age) + 1
 
     # Empty DataFrame to store results
-    columns = ['balance', 'income', 'savings', 'expenses', 'withdrawal', 'age', 'fi']
+    columns = ['Age', 'Balance', 'Income', 'Savings', 'Expenses', 'Return on Investment', 'Safe Withdrawal', '% FI', 'FI']
     cashflow_table = pd.DataFrame(
         data=np.zeros((years, len(columns))),
         index=range(years),
         columns=columns
     )
+    # cashflow_table['FI'] = False
     cashflow_table.index.name = 'year'
 
     # Store initial balance
-    cashflow_table.iloc[0]['balance'] = initial_balance
+    cashflow_table.iloc[0]['Balance'] = initial_balance
 
     # Generate Cashflow table
     fi = False
@@ -66,39 +69,44 @@ def assumption_fi_forecast(
         # Calculate savings and expenses
         yearly_savings = savings_rate * income
         if i == 0 or expense_increase:
-            yearly_expenses = (1 - savings_rate) * income if not fi else cashflow_table.loc[i-1]['withdrawal']
+            yearly_expenses = (1 - savings_rate) * income if not fi else cashflow_table.loc[i-1]['Safe Withdrawal']
+            yearly_expenses = max(yearly_expenses, min_spending)
+            yearly_expenses = min(yearly_expenses, max_spending)
 
         # store data
-        cashflow_table.loc[i]['age'] = age + i
-        cashflow_table.loc[i]['income'] = income
-        cashflow_table.loc[i]['savings'] = yearly_savings
-        cashflow_table.loc[i]['expenses'] = yearly_expenses
+        cashflow_table.loc[i, 'Age'] = age + i
+        cashflow_table.loc[i, 'Income'] = income
+        cashflow_table.loc[i, 'Savings'] = yearly_savings
+        cashflow_table.loc[i, 'Expenses'] = yearly_expenses
 
         # If not the first year
         if i >= 1:
+            # Determine Return
+            cashflow_table.loc[i, 'Return on Investment'] = return_rate * cashflow_table.loc[i-1]['Balance']
             # Growth balance
-            cashflow_table.loc[i]['balance'] = (1 + return_rate) * cashflow_table.loc[i-1]['balance']
-            # Calculate potential withdrawal
-            cashflow_table.loc[i]['withdrawal'] = withdrawal_rate * cashflow_table.loc[i-1]['balance']
+            cashflow_table.loc[i, 'Balance'] = (1 + return_rate) * cashflow_table.loc[i-1]['Balance']
+            # Calculate safe withdrawal
+            cashflow_table.loc[i, 'Safe Withdrawal'] = withdrawal_rate * cashflow_table.loc[i-1]['Balance']
+            cashflow_table.loc[i, '% FI'] = 100.0 * cashflow_table.loc[i, 'Safe Withdrawal'] / cashflow_table.loc[i, 'Expenses']
 
             # Once withdrawal is greater than expenses, retire
-            if cashflow_table.loc[i]['withdrawal'] >= cashflow_table.loc[i-1]['expenses']:
+            if cashflow_table.loc[i, 'Safe Withdrawal'] >= cashflow_table.loc[i-1]['Expenses']:
                 fi = True
                 # Remove withdrawal from blance for expenses
-                cashflow_table.loc[i]['balance'] -= cashflow_table.loc[i]['withdrawal']
+                cashflow_table.loc[i, 'Balance'] -= cashflow_table.loc[i]['Safe Withdrawal']
 
         if fi:
             # stop income
             income = 0
         elif i > 0:
             # Add yearly savings
-            cashflow_table.loc[i]['balance'] += yearly_savings
+            cashflow_table.loc[i, 'Balance'] += yearly_savings
 
             # increase income a little for next year
             income = (1 + income_increase) * income
 
         # Store boolean
-        cashflow_table.loc[i]['fi'] = fi
+        cashflow_table.loc[i, 'FI'] = fi
 
     # Turn Index into date if data available
     if start:
